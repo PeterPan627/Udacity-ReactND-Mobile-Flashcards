4db556531615dfd28fb91ebc9fb387b67103b567
LeiMing627 <jackperry933jp@gmail.com>
2017-11-17 13:19:32 -0800

    added files needed for app logic

---
+++ utils/api.js
@@ -0,0 +1,19 @@
+export function getDecks() {
+  return;
+  // return all of the decks with their titles, questions and answers
+}
+
+export function getDeck(id) {
+  return;
+  // return a deck that matches the id argument
+}
+
+export function saveDeckTitle(title) {
+  return;
+  // take in a single title argument and add it to the decks
+}
+
+export function addCardToDeck(title, card) {
+  return;
+  // add card to a decks questions 
+}
---
+++ utils/colors.js
@@ -0,0 +1,5 @@
+export const candyAppleRed = '#d90429';
+export const pantoneRed = '#ef233c';
+export const grayBlue = '#8d99ae';
+export const gunmetal = '#2b2d42';
+export const antiFlashWhite = '#edf2f4';
