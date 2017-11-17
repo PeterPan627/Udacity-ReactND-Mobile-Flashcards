c8c635eeeb4f1d4b44d24ee28c7ae9f04c10c715
LeiMing627 <jackperry933jp@gmail.com>
2017-11-17 14:44:55 -0800

    navigation half implemented, ugly but works :v

--- App.js
+++ App.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import { grayBlue, candyAppleRed } from './utils/colors';
 import DeckListScreen from './components/DeckListScreen';
 import AddDeckScreen from './components/AddDeckScreen';
 import AddCardScreen from './components/AddCardScreen';
 import IndividualDeckScreen from './components/IndividualDeckScreen';
 import QuizScreen from './components/QuizScreen';
 
+const Tabs = TabNavigator({
+  Decks: {
+    screen: DeckListScreen,
+    navigationOptions: {
+      topBarLabel: 'Decks'
+    }
+  },
+  AddDeck: {
+    screen: AddDeckScreen,
+    navigationOptions: {
+      topBarLabel: 'Add New Deck'
+    }
+  }
+},
+{
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: candyAppleRed,
+    style: {
+      height: 50,
+      backgroundColor: grayBlue,
+      shadowColor: 'rgba(0, 0, 0, 0.4)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+});
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs
+  },
+  IndividualDeck: {
+    screen: IndividualDeckScreen,
+    navigationOptions: {
+      headerTintColor: candyAppleRed,
+      headerStyle: {
+        backgroundColor: grayBlue,
+      }
+    }
+  }
+});
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Flashcards Ready!</Text>
-        <QuizScreen />
+      <View style={{flex: 1}}>
+        <View style={{height: 100, backgroundColor: 'black'}}></View>
+        <MainNavigator/>
       </View>
     );
   }
@@ -19,9 +70,6 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
